a803512a3cdedbed4cf43573a033e3488894a36b
Johnathon Douglass <johnathondouglass@gmail.com>
2021-09-03 15:11:58 -0700

    added reset button to reset the game state

--- script.js
+++ script.js
@@ -1,85 +1,92 @@
 // NOTE: this example uses the chess.js library:
 // https://github.com/jhlywa/chess.js
 
+
+// const { Chess } = require('./chess.js')
 var board = null
-var game = new Chess()
+const game = new Chess()
 var $status = $('#status')
 var $fen = $('#fen')
 var $pgn = $('#pgn')
 
 function onDragStart (source, piece, position, orientation) {
-  // do not pick up pieces if the game is over
-  if (game.game_over()) return false
+    // do not pick up pieces if the game is over
+    if (game.game_over()) return false
 
-  // only pick up pieces for the side to move
-  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false
-  }
+    // only pick up pieces for the side to move
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false
+    }
 }
 
 function onDrop (source, target) {
-  // see if the move is legal
-  var move = game.move({
-    from: source,
-    to: target,
-    promotion: 'q' // NOTE: always promote to a queen for example simplicity
-  })
+    // see if the move is legal
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    })
 
-  // illegal move
-  if (move === null) return 'snapback'
+    // illegal move
+    if (move === null) return 'snapback'
 
-  updateStatus()
+    updateStatus()
 }
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
 function onSnapEnd () {
-  board.position(game.fen())
+    board.position(game.fen())
 }
 
 function updateStatus () {
-  var status = ''
+    var status = ''
 
-  var moveColor = 'White'
-  if (game.turn() === 'b') {
-    moveColor = 'Black'
-  }
-
-  // checkmate?
-  if (game.in_checkmate()) {
-    status = 'Game over, ' + moveColor + ' is in checkmate.'
-  }
-
-  // draw?
-  else if (game.in_draw()) {
-    status = 'Game over, drawn position'
-  }
-
-  // game still on
-  else {
-    status = moveColor + ' to move'
-
-    // check?
-    if (game.in_check()) {
-      status += ', ' + moveColor + ' is in check'
+    var moveColor = 'White'
+    if (game.turn() === 'b') {
+        moveColor = 'Black'
     }
-  }
 
-  $status.html(status)
-  $fen.html(game.fen())
-  $pgn.html(game.pgn())
+    // checkmate?
+    if (game.in_checkmate()) {
+        status = 'Game over, ' + moveColor + ' is in checkmate.'
+    }
+
+    // draw?
+    else if (game.in_draw()) {
+        status = 'Game over, drawn position'
+    } else { // game still on
+        status = moveColor + ' to move'
+
+        // check?
+        if (game.in_check()) {
+          status += ', ' + moveColor + ' is in check'
+        }
+    }
+
+    $status.html(status)
+    $fen.html(game.fen())
+    $pgn.html(game.pgn())
 }
 
 var config = {
-  draggable: true,
-  position: 'start',
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onSnapEnd: onSnapEnd
+    draggable: true,
+    position: 'start',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd
 }
 board = Chessboard('board', config)
 
+// this function is called after every move is made and updates the turn status,
+// FEN and PGN
 updateStatus()
 
-$('#resetBtn').on('click', board.start)
+// when the reset button is pressed it resets the game state,
+// and resets the status variables
+$('#resetBtn').on('click', function() {
+    board = Chessboard('board', config) // is there another way to do this?
+    game.reset()
+    updateStatus()
+}) 
