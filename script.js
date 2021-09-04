ef248a369cec386d50ab00110b571ad001a84819
Johnathon Douglass <johnathondouglass@gmail.com>
2021-09-04 15:46:12 -0700

    added opening theory for scotch steinitz variation

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // NOTE: this example uses the chess.js library:
 // https://github.com/jhlywa/chess.js
+//
+//
+// NOTE TO SELF: thinking about adding in arrows when i have the project finished
+// to allow users to see the piece continuation after the main theoretical moves
+// are completed or to draw an arrow when the user does the incorrect move
+//
+// https://github.com/mcba1n/chessboard-arrows
 
 
 // const { Chess } = require('./chess.js')
@@ -25,12 +32,19 @@ function onDrop (source, target) {
     var move = game.move({
         from: source,
         to: target,
+
+        // take off auto queen? but not necessary for opening theory
         promotion: 'q' // NOTE: always promote to a queen for example simplicity
     })
 
     // illegal move
     if (move === null) return 'snapback'
-
+    let attempt = game.history()
+    if (attempt.length % 2 !== 0 && attempt[attempt.length-1] === Nxd4Answer[attempt.length-1]) {
+        game.move(Nxd4Answer[attempt.length])
+    } else {
+        game.undo()
+    }
     updateStatus()
 }
 
@@ -40,10 +54,12 @@ function onSnapEnd () {
     board.position(game.fen())
 }
 
+// updates three variables which are the colour's turn, FEN, and PGN
+// after each move is made and prints to the HTML tags
 function updateStatus () {
     var status = ''
-
     var moveColor = 'White'
+
     if (game.turn() === 'b') {
         moveColor = 'Black'
     }
@@ -65,28 +81,36 @@ function updateStatus () {
         }
     }
 
+    // update html id tags
     $status.html(status)
     $fen.html(game.fen())
     $pgn.html(game.pgn())
 }
 
-var config = {
-    draggable: true,
-    position: 'start',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onSnapEnd: onSnapEnd
+function startGame() {
+    let config = {
+        draggable: true,
+        position: 'start',
+        onDragStart: onDragStart,
+        onDrop: onDrop,
+        onSnapEnd: onSnapEnd
+    }
+    board = Chessboard('board', config)
+    game.reset()
+    updateStatus()
 }
-board = Chessboard('board', config)
+
 
 // this function is called after every move is made and updates the turn status,
 // FEN and PGN
 updateStatus()
 
+// when page loads, initialize the chess board to the starting position
+$(document).ready(startGame)
+
 // when the reset button is pressed it resets the game state,
 // and resets the status variables
-$('#resetBtn').on('click', function() {
-    board = Chessboard('board', config) // is there another way to do this?
-    game.reset()
-    updateStatus()
-}) 
+$('#resetBtn').click(startGame)
+
+const Nxd4Answer = ["e4", "e5", "Nf3", "Nc6", "d4", "exd4", "Nxd4", "Nxd4", "Qxd4", "d6", "Nc3", "Nf6", "f3", "Be7", "Be3", "O-O", "O-O-O"]
+
