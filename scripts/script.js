554ca6297aa3159743b3b98e7a4011bc0acf6321
Johnathon Douglass <johnathondouglass@gmail.com>
2021-09-07 21:30:42 -0700

    added more opening variations

--- scripts/script.js
+++ scripts/script.js
@@ -162,6 +162,17 @@ function showHint() {
     }
 }
 
+function flipBoard() {
+    // NEED TO FIX PIECE HIGHLIGHTING WHEN FLIPPING THE BOARD
+    if (board.orientation() === "white") {
+        board.orientation("black")
+        $board.find('.square-' + squareToHighlight).addClass('highlight-'+colorToHighlight)
+    } else {
+        board.orientation("white")
+        $board.find('.square-' + squareToHighlight).addClass('highlight-'+colorToHighlight)
+    }
+}
+
 function startGame() {
     let config = {
         draggable: true,
@@ -192,6 +203,8 @@ $('#resetBtn').click(startGame)
 
 $('#hintBtn').click(showHint)
 
+$('#flipBoardBtn').click(flipBoard)
+
 // scotch game lines
 const scotchClassical_Be3 = ["e4", "e5", "Nf3", "Nc6", "d4", "exd4", "Nxd4", "Bc5", "Be3", "Qf6", "c3", "Nge7", "Bc4", "Ne5", "Be2", "Qg6", "O-O", "d6"]
 const potterVariation = ["e4", "e5", "Nf3", "Nc6", "d4", "exd4", "Nxd4", "Bc5", "Nb3", "Bb6", "Nc3", "Qf6", "Qe2", "Nge7", "Be3", "O-O", "O-O-O", "d6"]
@@ -199,8 +212,23 @@ const scotchIntermezzo = ["e4", "e5", "Nf3", "Nc6", "d4", "exd4", "Nxd4", "Bc5",
 const steinitzVariation = ["e4", "e5", "Nf3", "Nc6", "d4", "exd4", "Nxd4", "Nxd4", "Qxd4", "d6", "Nc3", "Nf6", "f3", "Be7", "Be3", "O-O", "O-O-O"]
 const scotchGambit = []
 
-// sicilian defence lines
-const sicilianNajdorf = []
+// sicilian najdorf defence lines
+const sicilianNajdorfClassicalMainLine = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "a6", "Bg5", "e6", "f4", "Be7", "Qf3", "Qc7", "O-O-O", "Nbd7", "g4"]
+const sicilianNajdorfArgentine = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "a6", "Bg5", "e6", "f4", "Be7", "Qf3", "Qc7", "h6", "Bh4", "g5", "fxg5", "Nfd7"] // maybe not include this one
+const sicilianNajdorfEnglishAttack = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "a6", "Be3", "e5", "Nb3", "Be6", "f3"]
+const sicilianNajdorfLipnitzkyAttack = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "a6", "Bc4", "e6", "Bb3", "b5"]
+const sicilianNajdorfOpocensky = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "a6", "Be2", "e6"] // maybe not include this one
+const sicilianNajdorfAmsterdam = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "a6", "f4", "e5", "Nf3", "Nbd7", "a4", "Be7", "Bd3", "O-O"]
+const sicilianNajdorfAdams = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "a6", "h3", "e5", "Nde2", "h5"]
+
+// sicilian dragon defence lines
+const sicilianDragonYugoslav = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "g6", "Be3", "Bg7", "f3", "O-O", "Qd2", "Nc6", "Bc4", "Bd7", "O-O-O", "Rc8", "Bb3", "Ne5", "h4", "Nc4", "Bxc4", "Rxc4", "h5", "Nxh5", "g4", "Nf6", "Bh6"]
+const sicilianDragonClassical = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "g6", "Be2", "Bg7", "Be3", "Nc6", "O-O", "O-O", "Nb3", "d5"]
+const sicilianDragonLevenfish = ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "g6", "f4", "Nc6", "e5"]
+
+// sicilian defence classical
+
+
 
 // openings menu scripts
 // NOTE TO SELF: need to fix the arrow rotation
@@ -246,5 +274,13 @@ $("#steinitz-variation").click(function() {
 });
 
 $("#sicilian-defence").click(function() {
-    $("#sicilian-show").toggleClass("show");
+    $("#sicilian-defence-show").toggleClass("show");
+});
+
+$("#open-sicilian").click(function() {
+    $("#open-sicilian-show").toggleClass("show");
+});
+
+$("#queen-pawn-opening").click(function() {
+    $("#queen-pawn-show").toggleClass("show");
 });
