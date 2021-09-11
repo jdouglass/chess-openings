4bd08542a2985db0429dd767a22e150646262215
Johnathon Douglass <johnathondouglass@gmail.com>
2021-09-11 16:03:53 -0700

    finished all opening menu variations

--- scripts/menuJQueryFunctions.js
+++ scripts/menuJQueryFunctions.js
@@ -171,3 +171,68 @@ $("#owens-defence").click(function() {
 $("#queen-pawn-opening").click(function() {
     $("#queen-pawn-show").toggleClass("show");
 });
+$("#closed-game").click(function() {
+    $("#closed-game-show").toggleClass("show");
+});
+$("#queens-gambit").click(function() {
+    $("#queens-gambit-show").toggleClass("show");
+});
+$("#queens-gambit-accepted").click(function() {
+    $("#q-gambit-accepted-show").toggleClass("show");
+});
+$("#queens-gambit-declined").click(function() {
+    $("#q-gambit-declined-show").toggleClass("show");
+});
+$("#slav-defence").click(function() {
+    $("#slav-defence-show").toggleClass("show");
+});
+$("#semi-slav-defence").click(function() {
+    $("#semi-slav-defence-show").toggleClass("show");
+});
+$("#chigorin-defence").click(function() {
+    $("#chigorin-show").toggleClass("show");
+});
+$("#blackmar-diemer-gambit").click(function() {
+    $("#blackmar-diemer-show").toggleClass("show");
+});
+$("#indian-game").click(function() {
+    $("#indian-game-show").toggleClass("show");
+});
+$("#nimzo-indian-defence").click(function() {
+    $("#nimzo-indian-show").toggleClass("show");
+});
+$("#nimzo-rubinstein").click(function() {
+    $("#nimzo-rubinstein-show").toggleClass("show");
+});
+$("#nimzo-classical").click(function() {
+    $("#nimzo-classical-show").toggleClass("show");
+});
+$("#queens-indian-defence").click(function() {
+    $("#queens-indian-show").toggleClass("show");
+});
+$("#grunfeld-defence").click(function() {
+    $("#grunfeld-show").toggleClass("show");
+});
+$("#kings-indian-defence").click(function() {
+    $("#kings-indian-show").toggleClass("show");
+});
+$("#benoni-defence").click(function() {
+    $("#benoni-show").toggleClass("show");
+});
+$("#benko-gambit").click(function() {
+    $("#benko-show").toggleClass("show");
+});
+$("#dutch-defence").click(function() {
+    $("#dutch-show").toggleClass("show");
+});
+$("#q-pawn-modern-defence").click(function() {
+    $("#q-pawn-modern-defence-show").toggleClass("show");
+});
+$("#englund-gambit").click(function() {
+    $("#englund-show").toggleClass("show");
+});
+$("#english-opening").click(function() {
+    $("#english-opening-show").toggleClass("show");
+});
+
+
